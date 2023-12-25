8c74c8f95e3149ddca1aa6a9073fb6981a8421f3
Wally Yang <wallyyang28@gmail.com>
2023-12-25 17:06:53 -0500

    tutorial02 base code

--- leptjson.c
+++ leptjson.c
@@ -1,5 +1,5 @@
 #include <assert.h> /* assert() */
-#include <stdlib.h> /* NULL */
+#include <stdlib.h> /* NULL, strtod() */
 
 #include "leptjson.h"
 
@@ -43,13 +43,24 @@ static int lept_parse_false(lept_context* c, lept_value* v) {
 		return LEPT_PARSE_OK;
 }
 
+static int lept_parse_number(lept_context* c, lept_value* v) {
+		char* end;
+		/* \TODO validate number */
+		v->n = strtod(c->json, &end);
+		if (c->json == end)
+				return LEPT_PARSE_INVALID_VALUE;
+		c->json = end;
+		v->type = LEPT_NUMBER;
+		return LEPT_PARSE_OK;
+}
+
 static int lept_parse_value(lept_context* c, lept_value* v) {
 		switch (*c->json) {
 		case 'n':  return lept_parse_null(c, v);
 		case 't':  return lept_parse_true(c, v);
 		case 'f':  return lept_parse_false(c, v);
+		default:   return lept_parse_number(c, v);
 		case '\0': return LEPT_PARSE_EXPECT_VALUE;
-		default:   return LEPT_PARSE_INVALID_VALUE;
 		}
 }
 
@@ -77,4 +88,9 @@ int lept_parse(lept_value *v, const char *json) {
 lept_type lept_get_type(const lept_value *v) {
 		assert(v != NULL);
 		return v->type;
+}
+
+double lept_get_number(const lept_value* v) {
+		assert(v != NULL && v->type == LEPT_NUMBER);
+		return v-> n;
 }
