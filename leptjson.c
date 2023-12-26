0350738150fbaaecc2712ce633a8b1e1003a5114
Wally Yang <wallyyang28@gmail.com>
2023-12-25 22:30:42 -0500

    Refactor lept_parse
    Summary:
    As titled.
    
    Test Plan:
    ```
    -> % ./leptjson_test
    77/77 (100.00) passed
    ```
    
    Tasks:
    
    Tags:

--- leptjson.c
+++ leptjson.c
@@ -77,17 +77,14 @@ int lept_parse(lept_value *v, const char *json) {
     c.json = json;
     v->type = LEPT_NULL;
     lept_parse_whitespace(&c);
-    result = lept_parse_value(&c, v);
 
-    if (result != LEPT_PARSE_OK) {
-        return result;
+    if ((result = lept_parse_value(&c, v)) == LEPT_PARSE_OK) {
+        lept_parse_whitespace(&c);
+        if (*c.json != '\0') {
+            return LEPT_PARSE_ROOT_NOT_SINGULAR;
+        }
     }
-
-    lept_parse_whitespace(&c);
-    if (*c.json != '\0') {
-        return LEPT_PARSE_ROOT_NOT_SINGULAR;
-    }
-    return LEPT_PARSE_OK;
+    return result;
 }
 
 lept_type lept_get_type(const lept_value *v) {
